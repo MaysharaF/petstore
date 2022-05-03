e15305ac8133ae107ce2661984d1fba261f524c2
MaysharaF <mayshara@alunos.utfpr.edu.br>
2022-05-02 21:30:02 -0300

    Removendo construtores, getters, setters, toStrings e hash para utilizar @Data no cartItem

--- src/main/java/org/agoncal/application/petstore/domain/CartItem.java
+++ src/main/java/org/agoncal/application/petstore/domain/CartItem.java
@@ -3,16 +3,19 @@ package org.agoncal.application.petstore.domain;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
+import lombok.Data;
+
 /**
  * @author Antonio Goncalves
  *         http://www.antoniogoncalves.org
  *         --
  */
 
+@Data
 public class CartItem {
 
     // ======================================
-    // =             Attributes             =
+    // = Attributes =
     // ======================================
 
     @NotNull
@@ -22,74 +25,10 @@ public class CartItem {
     private Integer quantity;
 
     // ======================================
-    // =            Constructors            =
-    // ======================================
-
-    public CartItem(Item item, Integer quantity) {
-        this.item = item;
-        this.quantity = quantity;
-    }
-
-    // ======================================
-    // =              Public Methods        =
+    // = Public Methods =
     // ======================================
 
     public Float getSubTotal() {
         return item.getUnitCost() * quantity;
-    }
-
-    // ======================================
-    // =         Getters & setters          =
-    // ======================================
-
-    public Item getItem() {
-        return item;
-    }
-
-    public void setItem(Item item) {
-        this.item = item;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    // ======================================
-    // =   Methods hash, equals, toString   =
-    // ======================================
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        CartItem cartItem = (CartItem) o;
-
-        if (!item.equals(cartItem.item)) return false;
-        if (!quantity.equals(cartItem.quantity)) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = item.hashCode();
-        result = 31 * result + quantity.hashCode();
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder();
-        sb.append("CartItem");
-        sb.append("{item='").append(item).append('\'');
-        sb.append(", quantity='").append(quantity).append('\'');
-        sb.append('}');
-        return sb.toString();
     }
 }
