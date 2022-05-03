0bb209da20f342b25d808c824e998629e03f5810
MaysharaF <mayshara@alunos.utfpr.edu.br>
2022-05-02 21:34:45 -0300

    Removendo construtores, getters, setters e metodos hash, equalse toString para usar Data do lambok

--- src/main/java/org/agoncal/application/petstore/domain/Customer.java
+++ src/main/java/org/agoncal/application/petstore/domain/Customer.java
@@ -4,6 +4,8 @@ import org.agoncal.application.petstore.constraint.Email;
 import org.agoncal.application.petstore.constraint.Login;
 import org.agoncal.application.petstore.exception.ValidationException;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
@@ -27,10 +29,11 @@ import java.util.GregorianCalendar;
         @NamedQuery(name = Customer.FIND_ALL, query = "SELECT c FROM Customer c")
 })
 @XmlRootElement
+@Data
 public class Customer implements Serializable {
 
     // ======================================
-    // =             Attributes             =
+    // = Attributes =
     // ======================================
 
     @Id
@@ -64,7 +67,7 @@ public class Customer implements Serializable {
     private Integer age;
 
     // ======================================
-    // =             Constants              =
+    // = Constants =
     // ======================================
 
     public static final String FIND_BY_LOGIN = "Customer.findByLogin";
@@ -72,24 +75,7 @@ public class Customer implements Serializable {
     public static final String FIND_ALL = "Customer.findAll";
 
     // ======================================
-    // =            Constructors            =
-    // ======================================
-
-    public Customer() {
-    }
-
-    public Customer(String firstname, String lastname, String login, String password, String email, Address address) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.login = login;
-        this.password = password;
-        this.email = email;
-        this.homeAddress = address;
-        this.dateOfBirth = new Date();
-    }
-
-    // ======================================
-    // =          Lifecycle Methods         =
+    // = Lifecycle Methods =
     // ======================================
 
     /**
@@ -116,14 +102,15 @@ public class Customer implements Serializable {
     }
 
     // ======================================
-    // =              Public Methods        =
+    // = Public Methods =
     // ======================================
 
     /**
      * Given a password, this method then checks if it matches the user
      *
      * @param pwd Password
-     * @throws ValidationException thrown if the password is empty or different than the one
+     * @throws ValidationException thrown if the password is empty or different than
+     *                             the one
      *                             store in database
      */
     public void matchPassword(String pwd) {
@@ -135,119 +122,4 @@ public class Customer implements Serializable {
             throw new ValidationException("Passwords don't match");
     }
 
-    // ======================================
-    // =         Getters & setters          =
-    // ======================================
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-
-    public Address getHomeAddress() {
-        return homeAddress;
-    }
-
-    public void setHomeAddress(Address homeAddress) {
-        this.homeAddress = homeAddress;
-    }
-
-    public Date getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(Date dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    // ======================================
-    // =   Methods hash, equals, toString   =
-    // ======================================
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Customer)) return false;
-
-        Customer customer = (Customer) o;
-
-        if (!login.equals(customer.login)) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        return login.hashCode();
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder();
-        sb.append("Customer");
-        sb.append("{id=").append(id);
-        sb.append(", login='").append(login).append('\'');
-        sb.append(", password='").append(password).append('\'');
-        sb.append(", firstname='").append(firstname).append('\'');
-        sb.append(", lastname='").append(lastname).append('\'');
-        sb.append(", telephone='").append(telephone).append('\'');
-        sb.append(", email='").append(email).append('\'');
-        sb.append(", homeAddress=").append(homeAddress);
-        sb.append(", dateOfBirth=").append(dateOfBirth);
-        sb.append(", age=").append(age);
-        sb.append('}');
-        return sb.toString();
-    }
 }
