93c5eb38a4b50b3590f7efc67510047a0969cd45
MaysharaF <mayshara@alunos.utfpr.edu.br>
2022-05-02 21:33:09 -0300

    Removendo construtores, getters, setters e metodos para utilizar Data da lib lambok

--- src/main/java/org/agoncal/application/petstore/domain/CreditCard.java
+++ src/main/java/org/agoncal/application/petstore/domain/CreditCard.java
@@ -7,6 +7,8 @@ import javax.persistence.Enumerated;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import lombok.Data;
+
 /**
  * @author Antonio Goncalves
  *         http://www.antoniogoncalves.org
@@ -14,10 +16,11 @@ import javax.validation.constraints.Size;
  */
 
 @Embeddable
+@Data
 public class CreditCard {
 
     // ======================================
-    // =             Attributes             =
+    // = Attributes =
     // ======================================
 
     @Column(name = "credit_card_number", length = 30)
@@ -32,77 +35,4 @@ public class CreditCard {
     @NotNull
     @Size(min = 1, max = 5)
     private String creditCardExpDate;
-
-    // ======================================
-    // =            Constructors            =
-    // ======================================
-
-    public CreditCard() {
-    }
-
-    public CreditCard(String creditCardNumber, CreditCardType creditCardType, String creditCardExpDate) {
-        this.creditCardNumber = creditCardNumber;
-        this.creditCardType = creditCardType;
-        this.creditCardExpDate = creditCardExpDate;
-    }
-
-    // ======================================
-    // =         Getters & setters          =
-    // ======================================
-
-    public String getCreditCardNumber() {
-        return creditCardNumber;
-    }
-
-    public void setCreditCardNumber(String creditCardNumber) {
-        this.creditCardNumber = creditCardNumber;
-    }
-
-    public CreditCardType getCreditCardType() {
-        return creditCardType;
-    }
-
-    public void setCreditCardType(CreditCardType creditCardType) {
-        this.creditCardType = creditCardType;
-    }
-
-    public String getCreditCardExpDate() {
-        return creditCardExpDate;
-    }
-
-    public void setCreditCardExpDate(String creditCardExpDate) {
-        this.creditCardExpDate = creditCardExpDate;
-    }
-
-    // ======================================
-    // =   Methods hash, equals, toString   =
-    // ======================================
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof CreditCard)) return false;
-
-        CreditCard that = (CreditCard) o;
-
-        if (!creditCardNumber.equals(that.creditCardNumber)) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        return creditCardNumber.hashCode();
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder();
-        sb.append("CreditCard");
-        sb.append("{creditCardNumber='").append(creditCardNumber).append('\'');
-        sb.append(", creditCardType=").append(creditCardType);
-        sb.append(", creditCardExpDate='").append(creditCardExpDate).append('\'');
-        sb.append('}');
-        return sb.toString();
-    }
 }
