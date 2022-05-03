a40211a75cdf06a6d79ee85b9e1b09ef6795100a
MaysharaF <mayshara@alunos.utfpr.edu.br>
2022-05-02 21:35:55 -0300

    Removendo construtores, getters, setters e metodos hash, equals e toString para usar Data do lambok em Item

--- src/main/java/org/agoncal/application/petstore/domain/Item.java
+++ src/main/java/org/agoncal/application/petstore/domain/Item.java
@@ -3,6 +3,8 @@ package org.agoncal.application.petstore.domain;
 import org.agoncal.application.petstore.constraint.NotEmpty;
 import org.agoncal.application.petstore.constraint.Price;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -15,6 +17,7 @@ import javax.xml.bind.annotation.XmlTransient;
  *         --
  */
 
+@Data
 @Entity
 @NamedQueries({
         @NamedQuery(name = Item.FIND_BY_PRODUCT_ID, query = "SELECT i FROM Item i WHERE i.product.id = :productId"),
@@ -25,7 +28,7 @@ import javax.xml.bind.annotation.XmlTransient;
 public class Item {
 
     // ======================================
-    // =             Attributes             =
+    // = Attributes =
     // ======================================
 
     @Id
@@ -48,110 +51,11 @@ public class Item {
     private Product product;
 
     // ======================================
-    // =             Constants              =
+    // = Constants =
     // ======================================
 
     public static final String FIND_BY_PRODUCT_ID = "Item.findByProductId";
     public static final String SEARCH = "Item.search";
     public static final String FIND_ALL = "Item.findAll";
 
-    // ======================================
-    // =            Constructors            =
-    // ======================================
-
-    public Item() {
-    }
-
-    public Item(String name, Float unitCost, String imagePath, Product product, String description) {
-        this.name = name;
-        this.unitCost = unitCost;
-        this.imagePath = imagePath;
-        this.product = product;
-        this.description = description;
-    }
-
-    // ======================================
-    // =         Getters & setters          =
-    // ======================================
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Float getUnitCost() {
-        return unitCost;
-    }
-
-    public void setUnitCost(Float unitCost) {
-        this.unitCost = unitCost;
-    }
-
-    public String getImagePath() {
-        return imagePath;
-    }
-
-    public void setImagePath(String imagePath) {
-        this.imagePath = imagePath;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    // ======================================
-    // =   Methods hash, equals, toString   =
-    // ======================================
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Item)) return false;
-
-        Item item = (Item) o;
-
-        if (!imagePath.equals(item.imagePath)) return false;
-        if (!name.equals(item.name)) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = name.hashCode();
-        result = 31 * result + imagePath.hashCode();
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder();
-        sb.append("Item");
-        sb.append("{id=").append(id);
-        sb.append(", name='").append(name).append('\'');
-        sb.append(", unitCost=").append(unitCost).append('\'');
-        sb.append(", imagePath='").append(imagePath).append('\'');
-        sb.append(", description='").append(description).append('\'');
-        sb.append('}');
-        return sb.toString();
-    }
 }
